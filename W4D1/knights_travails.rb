e64bf7cbffbafc5f975b2921b05acc8e54098d4f
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-20 14:01:44 -0700

    Add new_move_positions method

--- W4D1/knights_travails.rb
+++ W4D1/knights_travails.rb
@@ -1,9 +1,26 @@
-
+require_relative "poly_tree_node"
 
 class KnightPathFinder
 
+  def self.valid_moves(pos)
+    
+  end
+
   def initialize(pos)
-    @pos = pos
+    @considered_positions = [pos]
+    @root_node = PolyTreeNode.new(pos)
+    build_move_tree
+  end
+
+  def build_move_tree
+
+  end
+
+  def new_move_positions(pos)
+    KnightPathFinder.valid_moves(pos).select do |el|
+      
+    end
   end
 
 end
+
