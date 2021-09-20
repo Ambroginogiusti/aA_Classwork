ce767d0d28a6e0611a1542fd41e1ec21b74b939a
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-20 14:42:01 -0700

    Work on new_move_positions

--- W4D1/knights_travails.rb
+++ W4D1/knights_travails.rb
@@ -7,9 +7,12 @@ class KnightPathFinder
     row, col = pos
     all_moves = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
     all_moves.each do |position|
-      new_position = [row +  position.first, col + position.last]
-      valid_moves.push(new_position) if (new_position.first >= 0 && new_position.first <= 7) && (new_position.last >= 0 && new_position.last <= 7)
+      new_position = [row + position.first, col + position.last]
+      if (new_position.first >= 0 && new_position.first <= 7) && (new_position.last >= 0 && new_position.last <= 7)
+        valid_moves.push(new_position)
+      end
     end
+    valid_moves
   end
 
   def initialize(pos)
@@ -19,13 +22,22 @@ class KnightPathFinder
   end
 
   def build_move_tree
+    array = []
+    array << @root_node
+    until array.empty?
+      node = array.shift
+      new_move_positions(node.value).each do |move|
+        
+      end
 
+    end
   end
 
   def new_move_positions(pos)
-    KnightPathFinder.valid_moves(pos).select do |el|
-      
+    new_pos = KnightPathFinder.valid_moves(pos).select do |el|
+      !@considered_positions.include?(el)
     end
+    @considered_positions += new_pos
   end
 
 end
