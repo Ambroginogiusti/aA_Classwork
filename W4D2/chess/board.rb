d204505d07828353a62c8cdfadeae26001c6be85
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-21 15:59:40 -0700

    Define set pieces

--- W4D2/chess/board.rb
+++ W4D2/chess/board.rb
@@ -1,10 +1,38 @@
+require_relative "piece"
+
 class Board
+
+  attr_accessor :grid
 
   def initialize
     @grid = Array.new(8) {Array.new(8)}
   end
 
   def move_piece(start_pos, end_pos)
+
+  end
+
+  def set_pieces
+    self.grid[0][0] = Rook.new(:white, [0,0])
+    self.grid[0][7] = Rook.new(:white, [0,7])
+    self.grid[0][1] = Knight.new(:white, [0,1])
+    self.grid[0][6] = Knight.new(:white, [0,6])
+    self.grid[0][2] = Bishop.new(:white, [0,2])
+    self.grid[0][5] = Bishop.new(:white, [0,5])
+    self.grid[0][3] = Queen.new(:white, [0,3])
+    self.grid[0][4] = King.new(:white, [0,5])
+    self.grid[7][0] = Rook.new(:black, [7,0])
+    self.grid[7][7] = Rook.new(:black, [7,7])
+    self.grid[7][1] = Knight.new(:black, [7,1])
+    self.grid[7][6] = Knight.new(:black, [7,6])
+    self.grid[7][2] = Bishop.new(:black, [7,2])
+    self.grid[7][5] = Bishop.new(:black, [7,5])
+    self.grid[7][3] = Queen.new(:black, [7,3])
+    self.grid[7][4] = King.new(:black, [7,5])
+    (0..7).each do |i|
+      self.grid[1][i] = Pawn.new(:white, [1, i])
+      self.grid[6][i] = Pawn.new(:black, [6, i])
+    end
   end
 
 end
---
+++ W4D2/chess/piece.rb
@@ -0,0 +1,10 @@
+class Piece
+
+  attr_accessor :pos, :color
+
+  def initialize(color, pos)
+    @color = color
+    @pos = pos
+  end
+
+end
